944c82fcbcafc442d815e7bd8f41c012903189ea
Michael Gordon <cosmic.mick@gmail.com>
2022-08-10 20:49:44 +1000

    Added secrets_tenplate.py this is an example of what you need in your secrets.py, secrets.py is added to .gitignore file along with the venv directory, added notebood for testing google custom search method of searching for linkedin profiles

---
+++ src/secrets/secrets_template.py
@@ -0,0 +1,8 @@
+
+
+############################################
+#   Google Api Configurations
+#   https://developers.google.com/custom-search/v1/using_rest
+############################################
+google_customsearch_api_key = "<get your own>"
+google_programmable_search_engine_id = "<get your own>"
