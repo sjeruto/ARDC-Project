1cb3cf63950516c4a580377c0c09ed879e1b068f
naeer <naeer.amin@gmail.com>
2022-10-05 09:40:02 +1100

    Linkedin Scraper
    - Code for scraping linkedin profiles of lobbyist employees

---
+++ src/linkedin_scraper/variables.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+my_username = "ccsaas2022@gmail.com"
+my_password = "Ccmdsi2022."
+
+file_name = '/Users/naeeramin/Documents/UTS_3rd_semester/ilab2/linkedin_scraper/results.json' # file where the results will be saved
+
+# query = 'site:linkedin.com/in/ AND "Python" AND "C"'
+# query = 'site:linkedin.com/in/ AND ("Adrian Michael Dolahenty" OR "Kurrajong Strategic Counsel")'
+# query = 'site:linkedin.com/in/ AND ("Alex Cramb" OR "Crisis&Comms Co Pty Ltd")'
+# query = 'site:linkedin.com/in/ AND ("Aisling Acton" OR "Michelson Alexander Pty Ltd")'
